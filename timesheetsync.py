deb0f5a89a7e60de9bee1835873bdb4ba4c84029
Aaron Goldstein <aaronmgoldstein@gmail.com>
2020-04-02 17:56:07 -0700

    Add collection of time entries from both platforms

--- timesheetsync.py
+++ timesheetsync.py
@@ -1,4 +1,4 @@
-from toggl.TogglPy import Toggl
+from toggl.TogglPy import Toggl, Endpoints
 import argparse
 import harvest
 import datetime
@@ -8,18 +8,23 @@ def main(args):
     # create a Toggl object and set our API key
     toggl = Toggl()
     toggl.setAPIKey(args.toggl_key)
-
-    response = toggl.request("https://www.toggl.com/api/v8/clients")
-
-    # print the client name and id for each client in the response
-    # list of returned values can be found in the Toggl docs:
-    # https://github.com/toggl/toggl_api_docs/blob/master/chapters/clients.md
-    for client in response:
+    toggl_entries = []
+    for client in toggl.getClients():
         print("Client name: %s  Client id: %s" % (client['name'], client['id']))
-
+        for workspace in toggl.getWorkspaces():
+            print("workspace name: %s workspace id: %s" % (workspace['name'], workspace['id']))
+            toggl_entries.append(toggl.request(Endpoints.TIME_ENTRIES, {'start_date': '2020-01-01T00:00:00+00:00', 'end_date': '2020-04-01T00:00:00+00:00'}))
+            # for entry in toggl.request(Endpoints.TIME_ENTRIES, {'start_date': '2020-01-01T00:00:00+00:00', 'end_date': '2020-04-01T00:00:00+00:00'}):
+            #     print("entry description: %s Task id: %s" % (entry['description'], entry['id']))
     
-    client = harvest.Harvest("https://sirromsystems.harvestapp.com", account_id=args.harvest_account_id, personal_token=args.harvest_key)
-    print(client.get_day(day_of_the_year=datetime.date(2020, 2, 18).timetuple().tm_yday, year=2020)['day_entries'])
+    account = harvest.Harvest("https://sirromsystems.harvestapp.com", account_id=args.harvest_account_id, personal_token=args.harvest_key)
+    harvest_entries = []
+    for client in account.clients():
+        for project in account.projects_for_client(client['client']['id']):
+            print('project')
+            harvest_entries.append(account.timesheets_for_project(project['project']['id'], start_date='2020-01-01T00:00:00+00:00', end_date='2020-04-01T00:00:00+00:00'))
+            # for timesheet in account.timesheets_for_project(project['project']['id'], start_date='2020-01-01T00:00:00+00:00', end_date='2020-04-01T00:00:00+00:00'):
+            #     print('id %s notes %s spent_at %s hours %s' % (timesheet['day_entry']['id'], timesheet['day_entry']['notes'], timesheet['day_entry']['spent_at'], timesheet['day_entry']['hours']))
     
 
 if __name__ == "__main__":
